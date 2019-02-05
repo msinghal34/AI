aa08fc88f349d1fc9f804cc8ee8a9473b791dfaf
MAYANK SINGHAL <mayanksinghal308@gmail.com>
2019-02-06 01:05:34 +0530

    Assignment 2 Base Code

---
+++ Assignment2/Lab2_base/layers.py
@@ -0,0 +1,176 @@
+import numpy as np
+
+class FullyConnectedLayer:
+	def __init__(self, in_nodes, out_nodes):
+		# Method to initialize a Fully Connected Layer
+		# Parameters
+		# in_nodes - number of input nodes of this layer
+		# out_nodes - number of output nodes of this layer
+		self.in_nodes = in_nodes
+		self.out_nodes = out_nodes
+		# Stores the outgoing summation of weights * feautres 
+		self.data = None
+
+		# Initializes the Weights and Biases using a Normal Distribution with Mean 0 and Standard Deviation 0.1
+		self.weights = np.random.normal(0,0.1,(in_nodes, out_nodes))	
+		self.biases = np.random.normal(0,0.1, (1, out_nodes))
+		###############################################
+		# NOTE: You must NOT change the above code but you can add extra variables if necessary 
+
+	def forwardpass(self, X):
+		# print('Forward FC ',self.weights.shape)
+		# Input
+		# activations : Activations from previous layer/input
+		# Output
+		# activations : Activations after one forward pass through this layer
+		
+		n = X.shape[0]  # batch size
+		# INPUT activation matrix  		:[n X self.in_nodes]
+		# OUTPUT activation matrix		:[n X self.out_nodes]
+
+		###############################################
+		# TASK 1 - YOUR CODE HERE
+		raise NotImplementedError
+		###############################################
+		
+	def backwardpass(self, lr, activation_prev, delta):
+		# Input
+		# lr : learning rate of the neural network
+		# activation_prev : Activations from previous layer
+		# delta : del_Error/ del_activation_curr
+		# Output
+		# new_delta : del_Error/ del_activation_prev
+		
+		# Update self.weights and self.biases for this layer by backpropagation
+		n = activation_prev.shape[0] # batch size
+
+		###############################################
+		# TASK 2 - YOUR CODE HERE
+		raise NotImplementedError
+		###############################################
+
+class ConvolutionLayer:
+	def __init__(self, in_channels, filter_size, numfilters, stride):
+		# Method to initialize a Convolution Layer
+		# Parameters
+		# in_channels - list of 3 elements denoting size of input for convolution layer
+		# filter_size - list of 2 elements denoting size of kernel weights for convolution layer
+		# numfilters  - number of feature maps (denoting output depth)
+		# stride	  - stride to used during convolution forward pass
+		self.in_depth, self.in_row, self.in_col = in_channels
+		self.filter_row, self.filter_col = filter_size
+		self.stride = stride
+
+		self.out_depth = numfilters
+		self.out_row = int((self.in_row - self.filter_row)/self.stride + 1)
+		self.out_col = int((self.in_col - self.filter_col)/self.stride + 1)
+
+		# Stores the outgoing summation of weights * feautres 
+		self.data = None
+		
+		# Initializes the Weights and Biases using a Normal Distribution with Mean 0 and Standard Deviation 0.1
+		self.weights = np.random.normal(0,0.1, (self.out_depth, self.in_depth, self.filter_row, self.filter_col))	
+		self.biases = np.random.normal(0,0.1,self.out_depth)
+		
+
+	def forwardpass(self, X):
+		# print('Forward CN ',self.weights.shape)
+		# Input
+		# X : Activations from previous layer/input
+		# Output
+		# activations : Activations after one forward pass through this layer
+		n = X.shape[0]  # batch size
+		# INPUT activation matrix  		:[n X self.in_channels[0] X self.in_channels[1] X self.in_channels[2]]
+		# OUTPUT activation matrix		:[n X self.outputsize[0] X self.outputsize[1] X self.numfilters]
+
+		###############################################
+		# TASK 1 - YOUR CODE HERE
+		raise NotImplementedError
+		###############################################
+
+	def backwardpass(self, lr, activation_prev, delta):
+		# Input
+		# lr : learning rate of the neural network
+		# activation_prev : Activations from previous layer
+		# delta : del_Error/ del_activation_curr
+		# Output
+		# new_delta : del_Error/ del_activation_prev
+		
+		# Update self.weights and self.biases for this layer by backpropagation
+		n = activation_prev.shape[0] # batch size
+
+		###############################################
+		# TASK 2 - YOUR CODE HERE
+		raise NotImplementedError
+		###############################################
+	
+class AvgPoolingLayer:
+	def __init__(self, in_channels, filter_size, stride):
+		# Method to initialize a Convolution Layer
+		# Parameters
+		# in_channels - list of 3 elements denoting size of input for max_pooling layer
+		# filter_size - list of 2 elements denoting size of kernel weights for convolution layer
+
+		# NOTE: Here we assume filter_size = stride
+		# And we will ensure self.filter_size[0] = self.filter_size[1]
+		self.in_depth, self.in_row, self.in_col = in_channels
+		self.filter_row, self.filter_col = filter_size
+		self.stride = stride
+
+		self.out_depth = self.in_depth
+		self.out_row = int((self.in_row - self.filter_row)/self.stride + 1)
+		self.out_col = int((self.in_col - self.filter_col)/self.stride + 1)
+
+	def forwardpass(self, X):
+		# print('Forward MP ')
+		# Input
+		# X : Activations from previous layer/input
+		# Output
+		# activations : Activations after one forward pass through this layer
+		
+		n = X.shape[0]  # batch size
+		# INPUT activation matrix  		:[n X self.in_channels[0] X self.in_channels[1] X self.in_channels[2]]
+		# OUTPUT activation matrix		:[n X self.outputsize[0] X self.outputsize[1] X self.in_channels[2]]
+
+		###############################################
+		# TASK 1 - YOUR CODE HERE
+		raise NotImplementedError
+		###############################################
+
+
+	def backwardpass(self, alpha, activation_prev, delta):
+		# Input
+		# lr : learning rate of the neural network
+		# activation_prev : Activations from previous layer
+		# activations_curr : Activations of current layer
+		# delta : del_Error/ del_activation_curr
+		# Output
+		# new_delta : del_Error/ del_activation_prev
+		
+		n = activation_prev.shape[0] # batch size
+
+		###############################################
+		# TASK 2 - YOUR CODE HERE
+		raise NotImplementedError
+		###############################################
+
+
+# Helper layer to insert between convolution and fully connected layers
+class FlattenLayer:
+    def __init__(self):
+        pass
+    
+    def forwardpass(self, X):
+        self.in_batch, self.r, self.c, self.k = X.shape
+        return X.reshape(self.in_batch, self.r * self.c * self.k)
+
+    def backwardpass(self, lr, activation_prev, delta):
+        return delta.reshape(self.in_batch, self.r, self.c, self.k)
+
+
+# Helper Function for the activation and its derivative
+def sigmoid(x):
+	return 1 / (1 + np.exp(-x))
+
+def derivative_sigmoid(x):
+	return sigmoid(x) * (1 - sigmoid(x))
---
+++ Assignment2/Lab2_base/tasks.py
@@ -0,0 +1,95 @@
+import nn
+import numpy as np
+import sys
+
+from util import *
+from visualize import *
+from layers import *
+
+
+# XTrain - List of training input Data
+# YTrain - Corresponding list of training data labels
+# XVal - List of validation input Data
+# YVal - Corresponding list of validation data labels
+# XTest - List of testing input Data
+# YTest - Corresponding list of testing data labels
+
+def taskSquare(draw):
+	XTrain, YTrain, XVal, YVal, XTest, YTest = readSquare()
+	# Create a NeuralNetwork object 'nn1' as follows with optimal parameters. For parameter definition, refer to nn.py file.
+	# nn1 = nn.NeuralNetwork(out_nodes, alpha, batchSize, epochs)	
+	# Add layers to neural network corresponding to inputs and outputs of given data
+	# Eg. nn1.addLayer(FullyConnectedLayer(x,y))
+	###############################################
+	# TASK 2.1 - YOUR CODE HERE
+	raise NotImplementedError
+	###############################################
+	nn1.train(XTrain, YTrain, XVal, YVal, False, True)
+	pred, acc = nn1.validate(XTest, YTest)
+	print('Test Accuracy ',acc)
+	# Run script visualizeTruth.py to visualize ground truth. Run command 'python3 visualizeTruth.py 2'
+	# Use drawSquare(XTest, pred) to visualize YOUR predictions.
+	if draw:
+		drawSquare(XTest, pred)
+	return nn1, XTest, YTest
+
+
+def taskSemiCircle(draw):
+	XTrain, YTrain, XVal, YVal, XTest, YTest = readSemiCircle()
+	# Create a NeuralNetwork object 'nn1' as follows with optimal parameters. For parameter definition, refer to nn.py file.
+	# nn1 = nn.NeuralNetwork(out_nodes, alpha, batchSize, epochs)	
+	# Add layers to neural network corresponding to inputs and outputs of given data
+	# Eg. nn1.addLayer(FullyConnectedLayer(x,y))
+	###############################################
+	# TASK 2.2 - YOUR CODE HERE
+	raise NotImplementedError
+	###############################################
+	nn1.train(XTrain, YTrain, XVal, YVal, False, True)
+	pred, acc  = nn1.validate(XTest, YTest)
+	print('Test Accuracy ',acc)
+	# Run script visualizeTruth.py to visualize ground truth. Run command 'python3 visualizeTruth.py 4'
+	# Use drawSemiCircle(XTest, pred) to vnisualize YOUR predictions.
+	if draw:
+		drawSemiCircle(XTest, pred)
+	return nn1, XTest, YTest
+
+def taskMnist():
+	XTrain, YTrain, XVal, YVal, XTest, YTest = readMNIST()
+	# Create a NeuralNetwork object 'nn1' as follows with optimal parameters. For parameter definition, refer to nn.py file.
+	# nn1 = nn.NeuralNetwork(out_nodes, alpha, batchSize, epochs)	
+	# Add layers to neural network corresponding to inputs and outputs of given data
+	# Eg. nn1.addLayer(FullyConnectedLayer(x,y))
+	###############################################
+	# TASK 2.3 - YOUR CODE HERE
+	raise NotImplementedError	
+	###############################################
+	nn1.train(XTrain, YTrain, XVal, YVal, False, True)
+	pred, acc  = nn1.validate(XTest, YTest)
+	print('Test Accuracy ',acc)
+	return nn1, XTest, YTest
+
+def taskCifar10():	
+	XTrain, YTrain, XVal, YVal, XTest, YTest = readCIFAR10()
+	
+	XTrain = XTrain[0:5000,:,:,:]
+	XVal = XVal[0:1000,:,:,:]
+	XTest = XTest[0:1000,:,:,:]
+	YVal = YVal[0:1000,:]
+	YTest = YTest[0:1000,:]
+	YTrain = YTrain[0:5000,:]
+	
+	modelName = 'model.npy'
+	# # Create a NeuralNetwork object 'nn1' as follows with optimal parameters. For parameter definition, refer to nn.py file.
+	# # nn1 = nn.NeuralNetwork(out_nodes, alpha, batchSize, epochs)	
+	# # Add layers to neural network corresponding to inputs and outputs of given data
+	# # Eg. nn1.addLayer(FullyConnectedLayer(x,y))
+	# ###############################################
+	# # TASK 2.4 - YOUR CODE HERE
+	raise NotImplementedError	
+	###################################################
+	# return nn1,  XTest, YTest, modelName # UNCOMMENT THIS LINE WHILE SUBMISSION
+
+
+	nn1.train(XTrain, YTrain, XVal, YVal, True, True, loadModel=False, saveModel=True, modelName=modelName)
+	pred, acc = nn1.validate(XTest, YTest)
+	print('Test Accuracy ',acc)
